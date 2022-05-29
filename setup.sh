23ecbd8a125d22f1a99946ab80f60c7ed88e8b30
kajih <kajih@wsl>
2022-05-29 12:33:39 +0200

    check for stow in setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 git submodule init
 git submodule update
 
+if [[ ! $(command -v stow) ]]; then
+   echo "stow not installed"
+   exit 1
+fi
+
 mkdir -p ~/go/bin
 mkdir -p ~/go/src/github.com
 mkdir -p ~/bin
@@ -15,13 +20,15 @@ mkdir -p ~/bin
 # -PROFILE
 [[ -f ~/.profile ]] && rm ~/.profile
 
-if [[ -h ~/.config ]] ; then 
+if [[ -h ~/.config ]] ; then
   echo ".config is symlinked... Exiting!"
   exit 0
 fi
 
 [[ ! -d ~/.config ]] && mkdir ~/.config
-
 stow -t ~/ -S config
+
+# Re-link file to prevent error
+[[ -f ~/.tmux.conf ]] && rm ~/.tmux.conf
 ln -s $(pwd)/config/.tmux/.tmux.conf ~/
 
