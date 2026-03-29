7d13ba1f18c3a15d2760805795d99912d9fca2ad
KaJIH <kajih@wsl>
2026-03-29 14:09:12 +0200

    shell tools in install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ sudo ./prereq.sh
 sudo ./fonts.sh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 ./setup.sh
 
